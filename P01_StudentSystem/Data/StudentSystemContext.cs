d38610c1f92e7a16d941a6be956fe9d2beb49b3a
Yordan <yordan.koparanov@gmail.com>
2023-11-01 14:24:45 +0200

    Initial commit

---
+++ P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        private const string connectionString = $"Server=TOWER-XXI\\SQLEXPRESS;Database=StudentSystem;Integrated Security=True;";
+
+        public StudentSystemContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(connectionString);
+        //}
+
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Homework> Homeworks { get; set; }
+        public DbSet<StudentCourse> StudentsCourses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+        }
+
+    }
+}
